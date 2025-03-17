31d197350957e19f8e716c78b056e0580b388e42
MaryamJami1 <maryamchouhan373@gmail.com>
2025-03-18 01:08:05 +0500

    make some changes

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
-
-from routes import app
-
-
 import os
 
+# Import your app
+from routes import app
+
 if __name__ == "__main__":
-    # Use environment variables for production, debug=True for development
     port = int(os.environ.get("PORT", 8080))
     debug = os.environ.get("DEBUG", "False").lower() == "true"
     app.run(host="0.0.0.0", port=port, debug=debug)
